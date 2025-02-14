70176899134f16bddf4df148fb7ea7166c9b6a72
koby.noguchi@gmail.com <koby.noguchi@gmail.com>
2025-02-14 14:38:47 -0500

    http/css additions

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+# admin.site.register(Question) # Uncomment if tutorial 6 edit doesnt work
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+admin.site.register(Question, QuestionAdmin)
